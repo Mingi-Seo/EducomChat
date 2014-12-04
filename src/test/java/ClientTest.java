c8052150be8b1737f2d2de89039342d3135c3318
cmkim <https://github.com/Mingi-Seo/MICE-IT>
2014-12-04 19:50:31 +0900

    Initial Commit - Start with ServerTest, ClientTest

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Mingi-Seo on 2014-12-04.
+ */
+public class ClientTest {
+    @Test
+    public void testClient() {
+
+    }
+}
---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Mingi-Seo on 2014-12-04.
+ */
+public class ServerTest {
+    @Test
+    public void testServer() {
+
+    }
+}
