3c95db1f74db667e50c3e5cb7ded9cbf9fbc9b19
cmkim <https://github.com/Mingi-Seo/MICE-IT>
2014-12-12 16:18:51 +0900

    - EducomClient에서 GUI를 EducomGUI 클래스로 분리시킴. - GUITest 코드 생성

---
+++ src/main/java/EducomGUI.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Mingi-Seo on 2014-12-12.
+ */
+public class EducomGUI implements ActionListener, Runnable {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/test/java/GUITest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Mingu-Seo on 2014-12-12.
+ */
+public class GUITest {
+
+}
