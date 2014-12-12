abff89bb8f5aead4c23e6ef278ba2e181b4eefbb
cmkim <https://github.com/Mingi-Seo/MICE-IT>
2014-12-12 16:30:46 +0900

    - EducomGUI 생성자와 main 메서드 추가

--- src/main/java/EducomGUI.java
+++ src/main/java/EducomGUI.java
@@ -52,6 +52,75 @@ public class EducomGUI implements ActionListener, Runnable {
     // 상태 플래그
     boolean status;
 
+    public EducomGUI(String ip) {
+        this.ip = ip;
+
+        // 로그인 패널 구성
+        loginPanel = new JPanel();
+        // 레이아웃 설정
+        loginPanel.setLayout(new BorderLayout());
+        idInput = new JTextField(15);
+        loginButton = new JButton("로그인");
+        // 이벤트 리스너 등록
+        loginButton.addActionListener(this);
+        label1 = new JLabel("대화명");
+        // 패널에 위젯 구성
+        loginPanel.add(label1, BorderLayout.WEST);
+        loginPanel.add(idInput, BorderLayout.CENTER);
+        loginPanel.add(loginButton, BorderLayout.EAST);
+
+        // 로그아웃 패널 구성
+        logoutPanel = new JPanel();
+        // 레이아웃 설정
+        logoutPanel.setLayout(new BorderLayout());
+        label2 = new JLabel();
+        logoutButton = new JButton("로그아웃");
+        // 이벤트 리스너 등록
+        logoutButton.addActionListener(this);
+        // 패널에 위젯 구성
+        logoutPanel.add(label2, BorderLayout.CENTER);
+        logoutPanel.add(logoutButton, BorderLayout.EAST);
+
+        // 입력 패널 구성
+        msgPanel = new JPanel();
+        // 레이아웃 설정
+        msgPanel.setLayout(new BorderLayout());
+        msgInput = new JTextField(30);
+        // 이벤트 리스너 등록
+        msgInput.addActionListener(this);
+        exitButton = new JButton("종료");
+        exitButton.addActionListener(this);
+        // 패널에 위젯 구성
+        msgPanel.add(msgInput, BorderLayout.CENTER);
+        msgPanel.add(exitButton, BorderLayout.EAST);
+
+        // 로그인/로그아웃 패널 선택을 위한 카드 레이아웃 패널
+        tab = new JPanel();
+        clayout = new CardLayout();
+        tab.setLayout(clayout);
+        tab.add(loginPanel, "login");
+        tab.add(logoutPanel, "logout");
+
+        // 메인 프레임 구성
+        jframe = new JFrame("Educom");
+        msgOut = new JTextArea("", 10, 30);
+        // JTextArea의 내용을 수정하지 못하게 함. 즉, 출력 전용으로 사용
+        msgOut.setEditable(false);
+        // 수직 스크롤바는 항상 나타내고, 수평 스크롤바는 필요할 때만 나타나게 함
+        JScrollPane jsp = new JScrollPane(msgOut, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        jframe.add(tab, BorderLayout.NORTH);
+        jframe.add(jsp, BorderLayout.CENTER);
+        jframe.add(msgPanel, BorderLayout.SOUTH);
+        // 로그인 패널을 우선 표시
+        clayout.show(tab, "login");
+        // 프레임 크기 자동 설정
+        jframe.pack();
+        // 프레임 크기 조정 불가 설정
+        jframe.setResizable(false);
+        // 프레임 표시
+        jframe.setVisible(true);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -60,5 +129,9 @@ public class EducomGUI implements ActionListener, Runnable {
     @Override
     public void run() {
 
+    }
+
+    public static void main(String[] args) {
+        EducomGUI gui = new EducomGUI("127.0.0.1");
     }
 }
