db249b4a3e7e7bed378bdf83c8a9fdac07c330b8
Mingi <lg77126266@gmail.com>
2014-12-19 12:24:10 +0900

    - 초기 접속 시에 서버가 열려 있는 지, 닫혀 있는 지에 대해서 테스트

--- src/test/java/edu/educom/simplechat/server/core/SimpleChatServerCoreTest.java
+++ src/test/java/edu/educom/simplechat/server/core/SimpleChatServerCoreTest.java
@@ -1,7 +1,19 @@
 package edu.educom.simplechat.server.core;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by cmkim on 2014-12-13.
  */
 public class SimpleChatServerCoreTest {
+    @Test
+    public void testServerCoreAliveOrClose() throws Exception {
+        SimpleChatServerCore serverCore = new SimpleChatServerCore();
+
+        //  처음 시작할 때에는 서버가 닫혀 있음
+        assertEquals(false, serverCore.isAlive());
+    }
 }
